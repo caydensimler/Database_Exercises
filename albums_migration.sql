17331ff3acd65bb47fc20fb333c6202ab55c062f
Cayden <cayden.simler@hotmail.com>
2017-04-04 10:20:43 -0500

    added the exercise for the albums seeder to mass add albums.

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,14 +1,14 @@
 USE codeup_test_db;
 
-DROP IF EXISTS albums;
+DROP TABLE IF EXISTS albums;
 
 
 CREATE TABLE IF NOT EXISTS albums (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	artist VARCHAR(30) NOT NULL,
-	name VARCHAR (50) NOT NULL,
+	artist VARCHAR(150) NOT NULL DEFAULT 'NONE',
+	name VARCHAR (150) NOT NULL DEFAULT 'NONE',
 	release_date YEAR(4),
-	sales ,
-	genre VARCHAR
-
+	sales DECIMAL(12, 2),
+	genre CHAR(255) NOT NULL DEFAULT 'UNKNOWN',
+	PRIMARY KEY (id)
 );
