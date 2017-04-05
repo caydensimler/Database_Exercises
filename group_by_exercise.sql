15b616388e6556c332c3bbe9bbaf1c4d1ba742a3
Cayden <cayden.simler@hotmail.com>
2017-04-05 14:11:35 -0500

    Added the exercises for group by and limit.

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -1,29 +1,36 @@
 -- In your script, use DISTINCT to find the unique titles in the titles table. Your results should look like:
-SELECT DISTINCT title
-FROM titles;
+	SELECT DISTINCT title
+	FROM titles;
 
 -- Update the previous query to sort the results alphabetically.
-SELECT DISTINCT title
-FROM titles
-ORDER BY title;
+	SELECT DISTINCT title
+	FROM titles
+	ORDER BY title;
 
 -- Find your query for employees whose last names start and end with 'E'. 
 -- Update the query find just the unique last names that start and end with 'E' using GROUP BY.
-SELECT DISTINCT last_name
-FROM employees 
-WHERE last_name LIKE 'e%' AND last_name LIKE '%e'
-GROUP BY last_name;
+	SELECT last_name
+	FROM employees 
+	WHERE last_name LIKE 'e%' AND last_name LIKE '%e'
+	GROUP BY last_name;
 
 -- Update your previous query to now find unique combinations of first and last name where 
 -- the last name starts and ends with 'E'. You should get 846 rows.
-SELECT DISTINCT first_name, last_name
-FROM employees 
-WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
+	SELECT DISTINCT first_name, last_name
+	FROM employees 
+	WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
+
+	-- OR
+
+	SELECT first_name, last_name
+	FROM employees 
+	WHERE last_name LIKE 'e%' AND last_name LIKE '%e'
+	GROUP BY first_name, last_name;
 
 -- Find the unique last names with a 'q' but not 'qu'. You may use either DISTINCT or GROUP BY.
-SELECT DISTINCT last_name 
-FROM employees 
-WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'; 
+	SELECT DISTINCT last_name 
+	FROM employees 
+	WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'; 
 
 
 
